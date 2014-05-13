97243fc746e985c59be0e93bff86687f7cf6445a
Kris Brandow <kris.brandow@gmail.com>
2014-05-13 14:23:57 -0400

    silly commit
    Change-Id: Iaac79d1b18cfab6b0341bda06beb780ee72b8474

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
 	fmt.Println("Don't ask me I'll never tell.")
 	fmt.Println("I looked to you as it fell.")
 	fmt.Println("You were in my way")
+	fmt.Println("You were in my way")
 	fmt.Println("So here's my number")
+	fmt.Println("Before you came into my life, I missed you so bad.")
 	fmt.Println("Before you came into my life, I missed you so bad.")
 }
