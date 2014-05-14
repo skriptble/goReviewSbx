ac91b8f0ed7a0b9bb9af4bc3771cfe84071f2ad6
Kris Brandow <kris.brandow@gmail.com>
2014-05-14 16:52:22 -0400

    Breaking test. Whoops.

--- main_test.go
+++ main_test.go
@@ -3,10 +3,12 @@ package main
 import (
 	"testing"
 
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(t *testing.T) {
 	assert.Equal(t, "hello", "hello", "Hello should equal Hello")
 	main()
+	So(5, ShouldEqual, 6)
 }
