04bf92c27716fa08eb7ce104cb0b7e5a20346565
Kris Brandow <kris.brandow@gmail.com>
2014-05-14 18:06:39 -0400

    WHEEE.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("And so it begins")
+	fmt.Println("And so it begins")
 	fmt.Println("I threw a wish in the well.")
 	fmt.Println("Don't ask me I'll never tell.")
 	fmt.Println("I looked to you as it fell.")
